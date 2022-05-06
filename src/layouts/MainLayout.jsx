d5844dd318719ca4eb7e3f7c76e48bc96696c40b
Tayyab Ferozi <ferozitayyab@gmail.com>
2022-05-07 04:20:27 +0500

    finish

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const MainLayout = ({ children }) => {
+  return (
+    <div id="main-layout">
+      <div id="header" className="section">
+        <div className="page-container">
+          <img className="logo" src="/assets/imgs/logo.png" alt="logo" />
+
+          <div className="btn">
+            <div className="btn-text">Connect wallet</div>
+          </div>
+        </div>
+      </div>
+
+      <div id="main-body">
+        {children}
+        <img className="bg" src="/assets/imgs/bg.png" alt="bg" />
+        <img className="trees" src="/assets/imgs/bottom.png" alt="trees" />
+      </div>
+    </div>
+  );
+};
+
+export default MainLayout;
---
+++ src/pages/Landing.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+
+import MainLayout from "../layouts/MainLayout";
+
+const Landing = () => {
+  const [numberState, setNumberState] = useState(0);
+
+  const changeNumber = (payload) => {
+    return setNumberState((prevState) => {
+      if (payload === "inc") {
+        return prevState + 1;
+      } else if (payload === "dec") {
+        if (prevState < 2) {
+          return 1;
+        }
+        return prevState - 1;
+      }
+    });
+  };
+
+  return (
+    <MainLayout>
+      <div className="section" id="main">
+        <div className="page-container">
+          <div className="d-flex justify-content-center">
+            <div className="heading">Day One Sale</div>
+          </div>
+
+          <div className="mint-box">
+            <div className="nft-container">
+              <img src="/assets/imgs/nft.png" alt="nft" />
+            </div>
+
+            <div className="number-wrap">
+              <img
+                className="c-pointer"
+                src="/assets/vectors/arrow-left.svg"
+                alt="arrow-left"
+                onClick={() => {
+                  changeNumber("dec");
+                }}
+              />
+              <div className="number">{numberState}</div>
+              <img
+                className="c-pointer"
+                src="/assets/vectors/arrow-right.svg"
+                alt="arrow-right"
+                onClick={() => {
+                  changeNumber("inc");
+                }}
+              />
+            </div>
+
+            <div className="text-center my-3">
+              <div className="minted">0/5,5556 Minted</div>
+
+              <div className="d-flex align-items-center justify-content-center gap-3">
+                <div>Price:</div> <h3>0.01 ETH</h3>
+              </div>
+            </div>
+
+            <div className="btn mint-btn">Mint</div>
+          </div>
+        </div>
+      </div>
+    </MainLayout>
+  );
+};
+
+export default Landing;
